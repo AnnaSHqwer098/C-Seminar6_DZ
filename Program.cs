c6bd72fdc8b96b3ee8e06e36b0b289c616eea795
Anna <anams_s@mail.ru>
2022-11-07 12:53:09 +0300

    Add fix

--- Program.cs
+++ Program.cs
@@ -68,7 +68,7 @@ int FindElement (int[] collection){
 int[] GetArray(int size, int minValue, int maxValue){
     int[] result = new int[size];
     for (int i = 0; i < size; i++){
-        result[i] = new Random().Next(minValue, maxValue);
+        result[i] = new Random().Next(minValue, maxValue + 1);
     }
     return result;
 }
